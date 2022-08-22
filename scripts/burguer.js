2f67d4c0bc105f38afbed568811cdfc5aaa8a0a1
Matias Garcia <garciamatias159@gmail.com>
2022-08-22 18:30:45 -0300

    carrito final

--- scripts/burguer.js
+++ scripts/burguer.js
@@ -1,7 +1,7 @@
 // ARRAYS //
 
 let hamburguesas = [];
-let carrito = [];
+let carrito = JSON.parse(localStorage.getItem("carritoCompras")) || [] ;
 
 // CONSTRUCTORES //
 
@@ -16,146 +16,147 @@ class Hamburguesa {
     } 
 }
 
-// OBJETOS (Hamburguesas) //
+// OBJETOS //
 
 const fatJoe = new Hamburguesa ("joe", "Fat Joe", "Ingredientes: Triple medallon de carne, cheddar, panceta, salsa jovie, mayonesa y cebolla caramelizada ", 1300, "../img/joe.png");
-hamburguesas.push(fatJoe);
-const fatLovie = new Hamburguesa ("lovie", "Fat Lovie", "Ingredientes: Doble Medallon de carne, huevo, lechuga, tomate, salsa jovie, mayonesa, panceta y cheddar", 1500, "../img/lovieMenu.png");
-hamburguesas.push(fatLovie);
-const rita = new Hamburguesa ("rita", "Rita's (Veggie)", "Ingredientes: Un medallon de soja texturizada, pepinillos, tomate. lechuga, cheddar y salsa jovie ", 1500, "../img/rita.png");
-hamburguesas.push(rita);
-const dexterino = new Hamburguesa ("dexter", "Dexterino", "Ingredientes: Doble medallon de carne, extra panceta, cheddar y salsa jovie ", 1500, "../img/dexterino.png");
-hamburguesas.push(dexterino);
-const harrison = new Hamburguesa ("harrison", "Harrison (Kids)", "Ingredientes: una hamburguesa simple con cheddar y salsa jovie ", 1500, "../img/harrison.png");
-hamburguesas.push(harrison);
-const fatista = new Hamburguesa ("fatista", "Fatista", "Ingredientes: Cuadruple medallon de carne,queso roquefort, salsa jovie, cebolla caramelizada, panceta y lechuga", 1500, "../img/fatist.png");
-hamburguesas.push(fatista);
-const debras = new Hamburguesa ("debras", "Debra 's", "Ingredientes: Un medallon de carne de 150gr, lechuga, pan con semillas, cheddar y cebolla crispy y mayonesa", 1500, "../img/debras.png");
-hamburguesas.push(debras);
-const doakes = new Hamburguesa ("doakes", "Doakes", "Ingredientes: Pan negro, un medallon de carne, lechuga, tomate, cebolla, mayonesa, salsa jovie y pepinillos ", 1500, "../img/doake.png");
-hamburguesas.push(doakes);
+const fatLovie = new Hamburguesa ("lovie", "Fat Lovie", "Ingredientes: Doble Medallon de carne, huevo, lechuga, tomate, salsa jovie, mayonesa, panceta y cheddar", 1150, "../img/lovieMenu.png");
+const rita = new Hamburguesa ("rita", "Rita's (Veggie)", "Ingredientes: Un medallon de soja texturizada, pepinillos, tomate. lechuga, cheddar y salsa jovie ", 1200, "../img/rita.png");
+const dexterino = new Hamburguesa ("dexter", "Dexterino", "Ingredientes: Doble medallon de carne, extra panceta, cheddar y salsa jovie ", 1150, "../img/dexterino.png");
+const harrison = new Hamburguesa ("harrison", "Harrison (Kids)", "Ingredientes: una hamburguesa simple con cheddar y salsa jovie ", 750, "../img/harrison.png");
+const fatista = new Hamburguesa ("fatista", "Fatista", "Ingredientes: Cuadruple medallon de carne,queso roquefort, salsa jovie, cebolla caramelizada, panceta y lechuga", 1400, "../img/fatist.png");
+const debras = new Hamburguesa ("debras", "Debra 's", "Ingredientes: Un medallon de carne de 150gr, lechuga, pan con semillas, cheddar y cebolla crispy y mayonesa", 1200, "../img/debras.png");
+const doakes = new Hamburguesa ("doakes", "Doakes", "Ingredientes: Pan negro, un medallon de carne, lechuga, tomate, cebolla, mayonesa, salsa jovie y pepinillos ", 1200, "../img/doake.png");
+hamburguesas.push(fatJoe, fatLovie, dexterino, harrison, fatista, rita, doakes, debras );
 
-// LOCAL STORAGE // 
+// DESAFIO DESESTRUCTURAR ARRAY // 
+const [a, b, c, d] = hamburguesas;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
 
-if (localStorage.getItem("carritoCompras")){
-    carrito = JSON.parse(localStorage.getItem("carritoCompras"));
-}else{
-    console.log("Hola")
-    localStorage.setItem("carritoCompras", []);
-}
-
+// SPREAD //
+console.log(...hamburguesas);
 
 // PLANTILLAS //
+function print(){
 
-hamburguesas.forEach((element)=> {
-    let producto = document.getElementById("sectionHamburguesas");
-    let burguer = document.createElement("div");
-    burguer.innerHTML=
-            `<div class= " d-flex flex-column justify-content-between align-items-center mt-5">
-                <img class="imgBurguers mb-3" src="${element.imagen}" alt="">
-                <h2 class="titleFooter">${element.nombre}</h2>
-                <button type="button" class="btn btnColor btn-lg text-white" data-bs-toggle="modal" data-bs-target="#${element.id}">
-                Descripcion
-                </button>
-                <div class="modal fade modals" id="${element.id}" tabindex="-1" aria-labelledby="${element.id}Label" aria-hidden="true">
-                <div class = "modal-dialog">   
-                    <div class="modal-content">
-                        <div class="modal-header d-flex flex-column justify-content-between">
-                            <div class="d-flex justify-content-between align-items-center w-100">
-                                <h2 class="modal-title fontTitle" id="${element.id}Label">${element.nombre}</h2>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    hamburguesas.forEach((element)=> {
+        let producto = document.getElementById("sectionHamburguesas");
+        let burguer = document.createElement("div");
+        burguer.innerHTML=
+                `<div class= " d-flex flex-column justify-content-between align-items-center mt-5">
+                    <img class="imgBurguers mb-3" src="${element.imagen}" alt="">
+                    <h2 class="titleFooter">${element.nombre}</h2>
+                    <button type="button" class="btn btnColor btn-lg text-white" data-bs-toggle="modal" data-bs-target="#${element.id}">
+                    Descripcion
+                    </button>
+                    <div class="modal fade modals" id="${element.id}" tabindex="-1" aria-labelledby="${element.id}Label" aria-hidden="true">
+                    <div class = "modal-dialog">   
+                        <div class="modal-content">
+                            <div class="modal-header d-flex flex-column justify-content-between">
+                                <div class="d-flex justify-content-between align-items-center w-100">
+                                    <h2 class="modal-title fontTitle" id="${element.id}Label">${element.nombre}</h2>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <img class="imgBurguers mb-3" src="${element.imagen}" alt="${element.nombre}">
+                                <p class="fontP">${element.descripcion}</p>
                             </div>
-                            <img class="imgBurguers mb-3" src="${element.imagen}" alt="${element.nombre}">
-                            <p class="fontP">${element.descripcion}</p>
-                        </div>
-                        <div class="modal-body">
-                            <h2 class="fontTitle1">Dejanos tus datos asi agendamos tu pedido</h2>
-                            <input type=text name="nombre" placeholder="Tu nombre..">
-                            <input type=text name="direccion" placeholder="Direccion">
-                            <h2 class="fontModal mt-5">Forma de pago</h2>
-                            <div class="d-flex justify-content-evenly w-50 mt-2">
-                                <div class="d-flex w-50">
-                                    <label for="efectivo">Efectivo</label>
-                                    <input class="ms-1"  id="cash" type=checkbox value="efectivo">
+                            <div class="modal-body">
+                                <h2 class="fontTitle1">Dejanos tus datos asi agendamos tu pedido</h2>
+                                <input type=text name="nombre" id="name-${element.id}" placeholder="Tu nombre..">
+                                <input type=text name="direccion" id="direc-${element.id}"
+                                placeholder="Direccion">
+                                <h2 class="fontModal mt-5">Forma de pago</h2>
+                                <div class="d-flex justify-content-evenly w-50 mt-2">
+                                    <div class="d-flex w-50">
+                                        <label for="efectivo">Efectivo</label>
+                                        <input class="ms-1"  id="cash" type=checkbox value="efectivo">
+                                    </div>
+                                    <div class="d-flex justify-content-evenly w-50">  
+                                        <label for="transf">Transferencia</label>
+                                        <input type=checkbox id="transf" value="transf">
+                                    </div>
                                 </div>
-                                <div class="d-flex justify-content-evenly w-50">  
-                                    <label for="transf">Transferencia</label>
-                                    <input type=checkbox id="transf" value="transf">
+                                <h2 class="fontModal mt-5">¿Desea agregar esto a su combo?</h2>
+                                <div class="d-flex  justify-content-evenly w-50 mt-2">
+                                    <div class="d-flex flex-column justify-content-between align-items-center w-100">
+                                        <label for="bebidas">Gaseosa($200)</label>
+                                        <input type=checkbox id="">
+                                    </div>
+                                    <div class="d-flex flex-column justify-content-between align-items-center w-100">  
+                                        <label for="">Papas($200)</label>
+                                        <input type=checkbox id="">
+                                    </div>
                                 </div>
+                                <h2 class="fontModal mt-5">Alguna observacion que quieras agregar a tu hamburguesa</h2>
+                                <textarea class="w-50 h-100" id="obs-${element.id}" placeholder="Escribe aqui.."></textarea>
                             </div>
-                            <h2 class="fontModal mt-5">¿Desea agregar esto a su combo?</h2>
-                            <div class="d-flex  justify-content-evenly w-50 mt-2">
-                                <div class="d-flex flex-column justify-content-between align-items-center w-100">
-                                    <label for="bebidas">Gaseosa($200)</label>
-                                    <input type=checkbox id="bebida${element.id}">
-                                </div>
-                                <div class="d-flex flex-column justify-content-between align-items-center w-100">  
-                                    <label for="">Papas($200)</label>
-                                    <input type=checkbox id="papa${element.id}">
-                                </div>
+                            <div class="modal-footer d-flex justify-content-between">
+                                <h2 class="fontTitle">Precio: ${element.precio}$</h2>
+                                <button id="btn-${element.id}" type="button" class="btns btn btnColor text-white" data-bs-dismiss="modal" aria-label="Close">Añadir a mi pedido</button>
                             </div>
-                            <h2 class="fontModal mt-5">Alguna observacion que quieras agregar a tu hamburguesa</h2>
-                            <textarea class="w-50 h-100" id="observ" placeholder="Escribe aqui.."></textarea>
-                        </div>
-                        <div class="modal-footer d-flex justify-content-between">
-                            <h2 class="fontModal">Precio:${element.precio}$</h2>
-                            <button id="btn-${element.id}" type="button" class="btns btn btnColor text-white" data-bs-dismiss="modal" aria-label="Close">Añadir a mi pedido</button>
                         </div>
                     </div>
-                </div>
-                </div>
-            </div>`;
-    producto.appendChild(burguer);
-    let btnAdd = document.getElementById(`btn-${element.id}`);
-    btnAdd.addEventListener("click", () => {agregarCarrito(element)});
-    let cbx3 = document.getElementById(`papa${element.id}`);
-    let cbx4 = document.getElementById(`bebida${element.id}`)
-    cbx3.addEventListener('change' , () => {
-        console.log(cbx3.checked);
+                    </div>
+                </div>`;
+        producto.appendChild(burguer);
+        let btnAdd = document.getElementById(`btn-${element.id}`);
+        btnAdd.addEventListener("click", () => {agregarCarrito(element)});
     })
-    cbx4.addEventListener('change' , () => {
-        console.log(cbx4.checked);
-    })
-})
+}
 
 
 function agregarCarrito(producto){
     carrito.push(producto)
-    console.log(carrito)
     localStorage.setItem("carritoCompras", JSON.stringify(carrito));
 }
 
+function ver(array){
+    let modalCarrito = document.getElementById("modalBody");
+    let plantilla = ``; 
+    array.forEach((producto)=> {
+        let direccion = document.getElementById(`direc-${producto.id}`).value;
+        let nombre = document.getElementById(`name-${producto.id}`).value;
+        let observacion = document.getElementById(`obs-${producto.id}`).value;
+        plantilla+= 
+        `
+        <div class="d-flex w-100 justify-content-around align-items-start">
+            <img class="imgPedidosModal mb-3" src="${producto.imagen}" alt="">
+            <div class="d-flex w-100 flex-column justify-content-between align-items-start mt-3 ms-3">
+                <h3 class="fontTitle1">Nombre: ${nombre}</h3>
+                <h3 class="fontTitle1">Llevar a: ${direccion}</h3>
+                <h3 class="fontTitle1">Producto: ${producto.nombre}</h3>
+                <h3 class="fontTitle1">Gaseosa:</h3>
+                <h3 class="fontTitle1">Observaciones: "${observacion}"</h3>
+                <h3 class="fontTitle1">Precio: ${producto.precio}$</h3>
+            </div>       
+        </div>
+        `
+        modalCarrito.innerHTML = plantilla;
+    })
+}
 
-// BOTONES // 
+// BOTONES Y FUNCIONES// 
 
 let botonCarrito = document.getElementById("btnCarrito");
 botonCarrito.addEventListener("click", () => {
     ver(carrito);
 });
+botonCarrito.addEventListener("click", ()=> {
+    sumar(carrito);
+})
 
+let total = document.getElementById("modal-footer");
 
-let modalCarrito = document.getElementById("modalBody");
-
-function ver(array){
-    array.forEach((producto)=> {
-
-        modalCarrito.innerHTML = 
-        `
-        <div class="d-flex w-100 justify-content-around align-items-start">
-            <img class="imgPedidosModal mb-3" src="${producto.imagen}" alt="">
-            <div class="d-flex w-100 flex-column justify-content-between align-items-start mt-3 ms-2">
-                <h2 class="fontTitle1">Producto: ${producto.nombre}</h2>
-                <h2 class="fontTitle1">Gaseosa:</h2>
-                <h2 class="fontTitle1">Observaciones:</h2>
-                <h2 class="fontTitle1">Total: ${producto.precio}$</h2>
-            </div>       
-        </div>
-           
-        `
-
-    })
+function sumar(array){
+    let acum = 0
+    array.forEach((producto => {
+        acum += (producto.precio)
+        total.innerHTML = `  <div> <h3 class="fontTitle">Total: ${acum}$ </div>
+                             <button type="button" class="btn btnColor text-white" data-bs-dismiss="modal">Pedir</button>`;
+    }))
 }
 
-
-
+print();
 
--- scripts/pollo.js
+++ scripts/pollo.js
@@ -1,9 +1,9 @@
 // ARRAYS //
 
 let pollos = [];
-let carrito = [];
+let carrito = JSON.parse(localStorage.getItem("carritoCompras")) || [] ;
 
-// CLASES //
+// CONSTRUCTORES //
 
 class Pollo {
     constructor (id, nombre, descripcion, precio, imagen){
@@ -19,129 +19,120 @@ class Pollo {
 // OBJETOS //
 
 const iceTruck = new Pollo ("ice", "Ice Truck bucket", "Contiene: 6 alitas, 6 pechugas y dip de salsa barbacoa", 1250, "../img/iceTruck.png");
-pollos.push(iceTruck);
 const trinity = new Pollo ("trinity", "Trinity bucket", "Contiene: 6 alitas, 3 patitas, 7 pechugas y dip de salsa jovie", 1250, "../img/trinity.png");
-pollos.push(trinity)
-
-
-// LOCAL STORAGE // 
-
- if (localStorage.getItem("carritoCompras")){
-    carrito = JSON.parse(localStorage.getItem("carritoCompras"));
-}else{
-    console.log("Hola")
-    localStorage.setItem("carritoCompras", []);
-} 
+pollos.push(trinity, iceTruck)
 
 
 // PLANTILLAS //
 
-pollos.forEach((element)=> {
-    let producto = document.getElementById("sectionPollos");
-    let burguer = document.createElement("div");
-    burguer.innerHTML=
-            `<div class= " d-flex flex-column justify-content-between align-items-center mt-5">
-                <img class="imgBurguers mb-3" src="${element.imagen}" alt="">
-                <h2 class="titleFooter text-white">${element.nombre}</h2>
-                <button type="button" class="btn btnColor btn-lg text-white" data-bs-toggle="modal" data-bs-target="#${element.id}">
-                Descripcion
-                </button>
-                <div class="modal fade modals" id="${element.id}" tabindex="-1" aria-labelledby="${element.id}Label" aria-hidden="true">
-                <div class = "modal-dialog">   
-                    <div class="modal-content">
-                        <div class="modal-header d-flex flex-column justify-content-between">
-                            <div class="d-flex justify-content-between align-items-center w-100">
-                                <h2 class="modal-title fontTitle" id="${element.id}Label">${element.nombre}</h2>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+function imprimir(){
+    pollos.forEach((element)=> {
+        let producto = document.getElementById("sectionPollos");
+        let burguer = document.createElement("div");
+        burguer.innerHTML=
+                `<div class= " d-flex flex-column justify-content-between align-items-center mt-5">
+                    <img class="imgBurguers mb-3" src="${element.imagen}" alt="">
+                    <h2 class="titleFooter text-white">${element.nombre}</h2>
+                    <button type="button" class="btn btnColor btn-lg text-white" data-bs-toggle="modal" data-bs-target="#${element.id}">
+                    Descripcion
+                    </button>
+                    <div class="modal fade modals" id="${element.id}" tabindex="-1" aria-labelledby="${element.id}Label" aria-hidden="true">
+                    <div class = "modal-dialog">   
+                        <div class="modal-content">
+                            <div class="modal-header d-flex flex-column justify-content-between">
+                                <div class="d-flex justify-content-between align-items-center w-100">
+                                    <h2 class="modal-title fontTitle" id="${element.id}Label">${element.nombre}</h2>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <img class="imgBurguers mb-3" src="${element.imagen}" alt="${element.nombre}">
+                                <p class="fontP">${element.descripcion}</p>
                             </div>
-                            <img class="imgBurguers mb-3" src="${element.imagen}" alt="${element.nombre}">
-                            <p class="fontP">${element.descripcion}</p>
-                        </div>
-                        <div class="modal-body">
-                            <h2 class="fontTitle1">Dejanos tus datos asi agendamos tu pedido</h2>
-                            <input type=text name="nombre" placeholder="Tu nombre..">
-                            <input type=text name="direccion" placeholder="Direccion">
-                            <h2 class="fontModal mt-5">Forma de pago</h2>
-                            <div class="d-flex justify-content-evenly w-50 mt-2">
-                                <div class="d-flex w-50">
-                                    <label for="efectivo">Efectivo</label>
-                                    <input class="ms-1" type=checkbox value="efectivo">
+                            <div class="modal-body">
+                                <h2 class="fontTitle1">Dejanos tus datos asi agendamos tu pedido</h2>
+                                <input type=text name="nombre" id="name-${element.id}" placeholder="Tu nombre..">
+                                <input type=text name="direccion" id="direc-${element.id}" placeholder="Direccion">
+                                <h2 class="fontModal mt-5">Forma de pago</h2>
+                                <div class="d-flex justify-content-evenly w-50 mt-2">
+                                    <div class="d-flex w-50">
+                                        <label for="efectivo">Efectivo</label>
+                                        <input class="ms-1" type=checkbox value="efectivo">
+                                    </div>
+                                    <div class="d-flex justify-content-evenly w-50">  
+                                        <label for="transf">Transferencia</label>
+                                        <input type=checkbox value="transf">
+                                    </div>
                                 </div>
-                                <div class="d-flex justify-content-evenly w-50">  
-                                    <label for="transf">Transferencia</label>
-                                    <input type=checkbox value="transf">
+                                <h2 class="fontModal mt-5">¿Desea agregar esto a su combo?</h2>
+                                <div class="d-flex  justify-content-evenly w-50 mt-2 mb-4">
+                                    <div class="d-flex flex-column justify-content-between align-items-center w-100">
+                                        <label for="bebida">Gaseosa($200)</label>
+                                        <input type=checkbox value="bebida">
+                                    </div>
+                                    <div class="d-flex flex-column justify-content-between align-items-center w-100">  
+                                        <label for="papas">Papas($200)</label>
+                                        <input type=checkbox value="papas">
+                                    </div>
                                 </div>
+                                <h2 class="fontModal mt-5">Alguna observacion que quieras agregar a tu pollo</h2>
+                                <textarea class="w-50 h-100" id="obs-${element.id}" placeholder="Escribe aqui.."></textarea>
                             </div>
-                            <h2 class="fontModal mt-5">¿Desea agregar esto a su combo?</h2>
-                            <div class="d-flex  justify-content-evenly w-50 mt-2 mb-4">
-                                <div class="d-flex flex-column justify-content-between align-items-center w-100">
-                                    <label for="bebida">Gaseosa($200)</label>
-                                    <input type=checkbox value="bebida" id="cbx${element.id}">
-                                </div>
-                                <div class="d-flex flex-column justify-content-between align-items-center w-100">  
-                                    <label for="papas">Papas($200)</label>
-                                    <input type=checkbox value="papas" id="cbx2${element.id}">
-                                </div>
+                            <div class="modal-footer">
+                                <button type="button" id="btn-${element.id}" class="btns btn btnColor text-white" data-bs-dismiss="modal" aria-label="Close">Añadir a mi pedido</button>
                             </div>
-                            <h2 class="fontModal mt-5">Alguna observacion que quieras agregar a tu pollo</h2>
-                            <textarea class="w-50 h-100" placeholder="Escribe aqui.."></textarea>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" id="button-${element.id}" class="btns btn btnColor text-white" data-bs-dismiss="modal" aria-label="Close">Añadir a mi pedido</button>
                         </div>
                     </div>
-                </div>
-                </div>
-            </div>`;
-    producto.appendChild(burguer);
-    let btnCarrito = document.getElementById(`button-${element.id}`);
-    btnCarrito.addEventListener("click", () => {agregar(element)});
-    let cbx = document.getElementById(`cbx${element.id}`);
-    let cbx2 = document.getElementById(`cbx2${element.id}`)
-    cbx.addEventListener('change' , () => {
-        console.log(cbx.checked);
+                    </div>
+                </div>`;
+        producto.appendChild(burguer);
+        let btnCartP = document.getElementById(`btn-${element.id}`);
+        btnCartP.addEventListener("click",() =>  {agregarCarrito(element)})
     })
-    cbx2.addEventListener('change' , () => {
-        console.log(cbx2.checked);
-    })
-})
+}
 
-
-
-function agregar(producto){
+function agregarCarrito(producto){
     carrito.push(producto)
     console.log(carrito)
     localStorage.setItem("carritoCompras", JSON.stringify(carrito));
 }
 
-// BOTONES //
-
-
-let botonCarrito = document.getElementById("btnCarrito2");
-botonCarrito.addEventListener("click", () => {
-    ver(carrito);
-});
-
-
-let modalCarrito = document.getElementById("modalBody2");
-
 function ver(array){
+    let modalCarrito = document.getElementById("modalBody2");
+    let plantilla = ``; 
     array.forEach((producto)=> {
-      
-        modalCarrito.innerHTML = 
+        plantilla += 
         `
         <div class="d-flex w-100 justify-content-around align-items-start">
             <img class="imgPedidosModal mb-3" src="${producto.imagen}" alt="">
             <div class="d-flex w-100 flex-column justify-content-between align-items-start mt-3 ms-2">
-                <h2 class="fontTitle1">Producto: ${producto.nombre}</h2>
-                <h2 class="fontTitle1">Gaseosa:</h2>
-                <h2 class="fontTitle1">Observaciones:</h2>
-                <h2 class="fontTitle1">Total: ${producto.precio}$</h2>
+                <h3 class="fontTitle1">Producto: ${producto.nombre}</h3>
+                <h3 class="fontTitle1">Gaseosa:</h3>
+                <h3 class="fontTitle1">Precio: ${producto.precio}$</h3>
             </div>       
         </div>
-           
         `
-
+        modalCarrito.innerHTML = plantilla;
     })
 }
 
+// BOTONES Y FUNCIONES //
+
+let botonCarrito = document.getElementById("btnCarrito1");
+botonCarrito.addEventListener("click", () => {
+    ver(carrito);
+});
+botonCarrito.addEventListener("click", () => {
+    sumar(carrito);
+})
+
+let total = document.getElementById("modal-footer1");
+
+function sumar(array){
+    let acum = 0
+    array.forEach((producto => {
+        acum += (producto.precio)
+        total.innerHTML = `  <div> <h3 class="fontTitle">Total: ${acum}$ </div>
+                             <button type="button" class="btn btnColor text-white" data-bs-dismiss="modal">Pedir</button>`;
+    }))
+}
+
+imprimir();
